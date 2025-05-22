af86428ba1462d56c1ef0002ad28d7edb6fbe98a
João Hugo <sjoaohugo@gmail.com>
2025-05-22 11:57:32 -0300

    first commit

---
+++ src/main/java/com/github/jhugosc/calculadora_posfixada/CalculadoraPosfixadaApplication.java
@@ -0,0 +1,13 @@
+package com.github.jhugosc.calculadora_posfixada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraPosfixadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraPosfixadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jhugosc/calculadora_posfixada/CalculadoraPosfixadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jhugosc.calculadora_posfixada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraPosfixadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
